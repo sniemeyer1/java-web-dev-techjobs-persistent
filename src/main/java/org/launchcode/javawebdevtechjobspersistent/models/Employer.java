ab75428d69d5a530f72c4afb27265be2ecf7a85c
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-03-31 05:43:22 -0500

    models

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    private String location;
 
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String longerDescription;
+
+    public Skill() {
+    }
+
+    public String getLongerDescription() {
+        return longerDescription;
+    }
+
+    public void setLongerDescription(String longerDescription) {
+        this.longerDescription = longerDescription;
+    }
 }
